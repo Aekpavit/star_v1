e53cf2524671732e9e8014f32c9691e87dc11d82
mawpong <motareban@gmail.com>
2026-07-14 21:28:31 +0700

    add txt in btn
    page register i forget txt "register"

--- client/src/views/regis.vue
+++ client/src/views/regis.vue
@@ -130,7 +130,9 @@ const handleRegister = () => {
         <button
           type="submit"
           class="w-full rounded-md bg-orange-400 py-2.5 text-sm font-semibold text-white transition hover:bg-orange-500"
-        ></button>
+        >
+          register
+        </button>
 
         <p class="text-center text-xs text-slate-500">
           Already Have An Account ?
